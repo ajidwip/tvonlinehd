b3bd8cbea3fe6a30e598943c0a102421a1b80e8e
slametatria <aji.dwipangestu@atria.co.id>
2018-04-08 11:45:25 +0700

    fixed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Content, NavController, NavParams, AlertController } from 'ionic-angular';
 import { ChatPage } from "../chat/chat";
 
 @Component({
@@ -8,17 +8,16 @@ import { ChatPage } from "../chat/chat";
 })
 
 export class HomePage {
-
+  @ViewChild(Content) content: Content;
   username: string = '';
+  scroll: any = 0;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public alertCtrl: AlertController
-  ) {
+    public alertCtrl: AlertController) {
 
   }
-
   showAlert(title: string, message: string) {
     let alert = this.alertCtrl.create({
       title: title,
@@ -38,5 +37,22 @@ export class HomePage {
       this.showAlert('Error', 'Invalid Username');
     }
   }
-
+  ngAfterViewInit() {
+    this.content.ionScroll.subscribe((event) => {
+      this.scroll = event.scrollTop
+      console.log(this.scroll)
+      if(this.scroll == 0) {
+        document.getElementById('header').style.display = 'block'
+        document.getElementById('header-sticky').style.visibility = 'hidden'
+        document.getElementById('content').style.marginTop = "0px";
+        document.getElementById('news').style.paddingTop = "0px";
+      }
+      else {
+        document.getElementById('header').style.display = 'none'
+        document.getElementById('header-sticky').style.visibility = 'visible'
+        document.getElementById('content').style.marginTop = "-120px";
+        document.getElementById('news').style.paddingTop = "50px";
+      }
+    });
+  }
 }
